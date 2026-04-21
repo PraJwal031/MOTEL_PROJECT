6c54c1369f712859029ba557b47d60358babcebd
Prajwal031 <venkatprajwal1999@gmail.com>
2026-04-21 11:48:51 +0530

    Added loaded_at and quality rules for debug teat

--- sfDbtAws/models/bronze/bronze_listings.sql
+++ sfDbtAws/models/bronze/bronze_listings.sql
@@ -1,8 +1,14 @@
 {{ config(materialized='incremental') }}
 
 
-SELECT * FROM  {{ source('staging', 'listings') }}
+SELECT * , CURRENT_TIMESTAMP() AS loaded_at   FROM   
+{{ source('staging', 'listings') }}
 
 {% if is_incremental() %}
     WHERE CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
 {% endif %}
+
+
+
+{# select * from {{ref("bronze_listings")}}
+limit 30 #}
--- sfDbtAws/models/silver/silver_listings.sql
+++ sfDbtAws/models/silver/silver_listings.sql
@@ -4,7 +4,11 @@ SELECT
     LISTING_ID,
     HOST_ID,
     PROPERTY_TYPE,
-    ROOM_TYPE,
+    CASE 
+        WHEN UPPER(TRIM(ROOM_TYPE)) = 'ENTIRE HOME' THEN 'Entire home/apt'
+        WHEN UPPER(TRIM(ROOM_TYPE)) = 'PRIVATE ROOM' THEN 'Private room'
+        ELSE 'Unknown'
+    END AS ROOM_TYPE,
     CITY,
     COUNTRY,
     ACCOMMODATES,
@@ -12,8 +16,12 @@ SELECT
     BATHROOMS,
     PRICE_PER_NIGHT,
     {{ tag('CAST(PRICE_PER_NIGHT AS INT)') }} AS PRICE_PER_NIGHT_TAG,
-    CREATED_AT
+    CREATED_AT,
+    loaded_AT
 FROM 
     {{ ref('bronze_listings') }}
 
+
     
+{# select * from {{ ref('silver_listings')}}
+limit 50 #}
