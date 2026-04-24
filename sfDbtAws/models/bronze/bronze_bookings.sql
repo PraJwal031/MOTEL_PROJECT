9a70d0f192737a205b4fdff6754e21c464325bf4
Prajwal031 <venkatprajwal1999@gmail.com>
2026-04-24 19:39:21 +0530

    added data_genetaor and aws keys with python scripts

--- sfDbtAws/models/bronze/bronze_bookings.sql
+++ sfDbtAws/models/bronze/bronze_bookings.sql
@@ -5,4 +5,8 @@ SELECT * FROM  {{ source('staging', 'bookings') }}
 
 {% if is_incremental() %}
     WHERE CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
-{% endif %}
+{% endif %} 
+
+
+{# select * from {{ref('bronze_bookings')}}
+limit 10 #}
